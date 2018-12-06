eee6f7fb224cabb09b47482244fa54fdf61301c0
Quinn Wilson <c9@midworld.org>
2018-12-06 17:53:37 +0000

    hlint'ed

--- src/Day6.hs
+++ src/Day6.hs
@@ -2,12 +2,9 @@
      ( 
          day6
        , day6b
-       , collectRemovingCollision
      ) where
      
-import Data.List
-import qualified Data.Map as M
-import qualified Data.Set as S
+import Data.List (sortBy, groupBy, maximumBy, minimumBy)
 
 input = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9\n"
 
@@ -17,39 +14,38 @@ day6 input =  maximum
                 $ sortBy (\a b -> snd a `compare` snd b) 
                 $ map (\y -> (fst y, fst $ head $ snd y)) 
                 $ filter (\y -> snd y /=[] ) 
-                $ map (\y -> (y, closest' y parsedInput)) grid
+                $ map (\y -> (y, closest y parsedInput)) grid
     where grid = getGrid parsedInput
           parsedInput = parseInput input
-          set = map(\y -> (y,closest' y parsedInput)) grid
+          set = map(\y -> (y,closest y parsedInput)) grid
           
 
 day6b input = 0
 
+getGrid :: (Foldable t, Ord b, Num b, Enum b) => t (b, b) -> [(b, b)]
 getGrid input = grid min max
     where min = minimumBy coordCompare input
           max = maximumBy coordCompare input
-          grid (a,b) (c,d) = [ (x,y) | x <- [(a)..(c)], y <- [(b)..(d)]]  
+          grid (a,b) (c,d) = [ (x,y) | x <- [a..c], y <- [b..d]]  
           
+coordCompare :: (Ord a, Num a) => (a, a) -> (a, a) -> Ordering
 coordCompare (a, b) (c, d) = compare (a+b) (c+d) 
 
+distance :: Num a => (a, a) -> (a, a) -> a
 distance (a,b) (c,d) = abs (c-a) + abs (d-b)
 
-closest x xs  =  head $ sortBy (\(_,y) (_,z)  -> compare y z ) $ map (\y -> (y, distance x y) ) xs
-
+parseInput :: String -> [(Int, Int)]
 parseInput input = map ( ( \(x : [y]) -> (x, y)) 
                         . (map (\ x -> read x :: Int) 
                         . words 
                         . filter (/= ',')) )  
                         $ lines input
                         
-closest' x xs = (\y -> if (length y>1) then [] else (take 1 y) )
+closest :: (Ord b, Num b) => (b, b) -> [(b, b)] -> [((b, b), b)]
+closest x xs = (\y -> if length y>1 then [] else take 1 y )
                  $ head  
                  $ groupBy (\a b -> snd a == snd b)  
                  $ sortBy (\(_,y) (_,z)  -> compare y z ) 
                  $ map (\y -> (y, distance x y) ) xs
 
-collectRemovingCollision :: Ord k => k -> a -> M.Map k a -> M.Map k a
-collectRemovingCollision key el set 
-    | M.member key set == True = M.delete key set
-    | otherwise = M.insert key el set
     
--- test/Day6Spec.hs
+++ test/Day6Spec.hs
@@ -9,22 +9,7 @@ spec = do
     describe "Lib" $ 
         it "should run a noop test" $ 
             1 `shouldBe` 1
-    describe "collectRemovingCollision" $ do
-        it "should remove collision" $ do
-            let key = ((1,1),2)
-            let el = ((0,0),((1,1),2))
-            let set = M.fromList [(((1,1),2),((0,0),((1,1),2)))]
-            let expected = M.empty
-            collectRemovingCollision key el set `shouldBe` expected
             
-        it "should add non-collision" $ do
-            let key = ((1,1),2)
-            let el = ((0,0),((1,1),2))
-            let set = M.fromList [(((1,1),3),((0,0),((1,1),3)))]
-            let expected = M.fromList [(((1,1),2),((0,0),((1,1),2))), (((1,1),3),((0,0),((1,1),3))) ]
-            -- wow reading these is hard
-            collectRemovingCollision key el set `shouldBe` expected
-        
     describe "Day6" $ 
         it "should run a test case" $ do
             let  input = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9\n"
