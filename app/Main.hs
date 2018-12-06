7090820867618c9ddbe4f1c65b8b5774fe492a99
Quinn Wilson <c9@midworld.org>
2018-12-06 17:47:15 +0000

    day6 working - hack job

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,7 @@ import Day2
 import Day3
 import Day4
 import Day5
+import Day6
 
 
 main :: IO ()
@@ -32,8 +33,11 @@ main = do
     -- putStrLn ("day4: " ++ answer)
     -- -- let answer = show $ day4b contents
     -- -- putStrLn ("day4b: " ++ answer)
-    contents <- readFile "data/day5.txt"
-    let answer = show $ day5 contents
-    putStrLn ("day5: " ++ answer)
+    -- contents <- readFile "data/day5.txt"
+    -- let answer = show $ day5 contents
+    -- putStrLn ("day5: " ++ answer)
     -- let answer = show $ day5b contents
     -- putStrLn ("day5b: " ++ answer)
+    contents <- readFile "data/day6.txt"
+    let answer = show $ day6 contents
+    putStrLn ("day6: " ++ answer)
--- src/Day6.hs
+++ src/Day6.hs
@@ -2,10 +2,54 @@
      ( 
          day6
        , day6b
+       , collectRemovingCollision
      ) where
      
-import qualified Data.Set as Set
+import Data.List
+import qualified Data.Map as M
+import qualified Data.Set as S
 
-day6 input = 0
+input = "1, 1\n1, 6\n8, 3\n3, 4\n5, 5\n8, 9\n"
+
+day6 input =  maximum 
+                $ map length 
+                $ groupBy (\a b -> snd a == snd b) 
+                $ sortBy (\a b -> snd a `compare` snd b) 
+                $ map (\y -> (fst y, fst $ head $ snd y)) 
+                $ filter (\y -> snd y /=[] ) 
+                $ map (\y -> (y, closest' y parsedInput)) grid
+    where grid = getGrid parsedInput
+          parsedInput = parseInput input
+          set = map(\y -> (y,closest' y parsedInput)) grid
+          
 
 day6b input = 0
+
+getGrid input = grid min max
+    where min = minimumBy coordCompare input
+          max = maximumBy coordCompare input
+          grid (a,b) (c,d) = [ (x,y) | x <- [(a)..(c)], y <- [(b)..(d)]]  
+          
+coordCompare (a, b) (c, d) = compare (a+b) (c+d) 
+
+distance (a,b) (c,d) = abs (c-a) + abs (d-b)
+
+closest x xs  =  head $ sortBy (\(_,y) (_,z)  -> compare y z ) $ map (\y -> (y, distance x y) ) xs
+
+parseInput input = map ( ( \(x : [y]) -> (x, y)) 
+                        . (map (\ x -> read x :: Int) 
+                        . words 
+                        . filter (/= ',')) )  
+                        $ lines input
+                        
+closest' x xs = (\y -> if (length y>1) then [] else (take 1 y) )
+                 $ head  
+                 $ groupBy (\a b -> snd a == snd b)  
+                 $ sortBy (\(_,y) (_,z)  -> compare y z ) 
+                 $ map (\y -> (y, distance x y) ) xs
+
+collectRemovingCollision :: Ord k => k -> a -> M.Map k a -> M.Map k a
+collectRemovingCollision key el set 
+    | M.member key set == True = M.delete key set
+    | otherwise = M.insert key el set
+    
