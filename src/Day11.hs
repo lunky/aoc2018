bbf72ba2df460ce064eacab8f77fc1800b3f92b8
Quinn Wilson <c9@midworld.org>
2018-12-26 18:36:03 +0000

    day11 testing

--- src/Day11.hs
+++ src/Day11.hs
@@ -38,12 +38,6 @@ day11bt gmap point = (\(x,y) -> ((point, y),x))
                         $ map (\(x,y)-> (sumPowerLevelsMap gmap x,y)) 
                         $ map xByX $ squaresXByX point
     
--- day11bt input = fst $ maximumBy (\(a,b) (c,d) -> (compare b d)) $ map (\y -> (y, sumTbT' input y))  getGrid
-
-threeByThree (x,y) = [(x,y),   (x+1,y),   (x+2,y),
-                      (x,y+1), (x+1,y+1), (x+2,y+1),
-                      (x,y+2), (x+1,y+2), (x+2,y+2) ]
-                      
 xByX :: ((Int,Int),Int) -> ([(Int,Int)],Int)
 xByX ((x,y),gridSize) = ([(x+a,y+b) | a<-[0..gridSize-1], b<-[0..gridSize-1]],gridSize)
                       
@@ -55,8 +49,9 @@ getGridMap :: Int -> Map (Int,Int) Int
 getGridMap serial = Map.fromList $ map(\y -> (y,getPowerLevel serial y)) $ getGrid
 
 
--- sumTbT' serial start x = sum $ map (\y -> getPowerLevel serial y) $ xByX (start,x)
-gridMap = getGridMap 18
+--gridMap = getGridMap 18
 
 glookup :: Map (Int,Int) Int -> (Int,Int) -> Int 
-glookup map loc = gridMap Map.! loc
+glookup gmap loc = gmap Map.! loc
+
+
