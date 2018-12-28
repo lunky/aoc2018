006aa53e693678e15d34bddf931f8387706493a3
Quinn Wilson <c9@midworld.org>
2018-12-28 06:53:20 +0000

    day11b finally working

--- src/Day11.hs
+++ src/Day11.hs
@@ -4,14 +4,16 @@
        , day11b
        , getPowerLevel
        , sumTbT
-       , tryThree
+       , pointMaximum 
      ) where
      
 import Data.List (maximumBy)
-import Data.Map (Map)
-import qualified Data.Map as Map
      
 input = 8
+
+day11 input = fst $ maximumBy (\(a,b) (c,d) -> (compare b d)) $ map (\y -> (y, sumTbT input y))  getGrid
+
+day11b serial = (\((a,b),c,d) -> init $ tail $ show [a,b,c] ) $ maximumBy (\(_,_,a) (_,_,b) -> compare a b) $ map (\y ->(\(a,b) -> (y,a,b)) $ pointMaximum serial y) $ getGrid
 
 getPowerLevel :: Int -> (Int, Int) -> Int
 getPowerLevel serial (x,y) = hundredthsPlace (((rackId * y) + serial) * rackId) - 5
@@ -21,24 +23,12 @@ getPowerLevel serial (x,y) = hundredthsPlace (((rackId * y) + serial) * rackId) 
 sumPowerLevels :: Int -> [(Int, Int)] -> Int
 sumPowerLevels serial list = sum $ map (\y -> getPowerLevel serial y) list
 
-sumPowerLevelsMap :: Map (Int,Int) Int -> [(Int, Int)] -> Int
-sumPowerLevelsMap gridMap list = sum $ map (\y -> glookup gridMap y) list
-
-day11 input = fst $ maximumBy (\(a,b) (c,d) -> (compare b d)) $ map (\y -> (y, sumTbT input y))  getGrid
-
 getGrid :: [(Int,Int)]
 getGrid =  [ (x,y) | x <- [1..300], y <- [1..300]] 
 
+sumTbT :: Int -> (Int, Int) -> Int
 sumTbT serial start = sum $ map (\y -> getPowerLevel serial y) $ fst $ xByX (start,3)
 
-day11b serial = fst $ maximumBy (\(a,b) (c,d) -> compare b d) $ map (\y -> day11bt gmap y) $ getGrid
-    where gmap = getGridMap serial
-
-day11bt gmap point = (\(x,y) -> ((point, y),x)) 
-                        $ maximumBy (\(a,b) (c,d) ->(compare a c)) 
-                        $ map (\(x,y)-> (sumPowerLevelsMap gmap x,y)) 
-                        $ map xByX $ squaresXByX point
-    
 xByX :: ((Int,Int),Int) -> ([(Int,Int)],Int)
 xByX ((x,y),gridSize) = ([(x+a,y+b) | a<-[0..gridSize-1], b<-[0..gridSize-1]],gridSize)
                       
@@ -46,30 +36,16 @@ squaresXByX :: (Int, Int) -> [((Int,Int),Int)]
 squaresXByX (x,y) = takeWhile (\((_,_),a) -> (a+x <= bound+1) && (a+y <= bound+1)) $ map (\z-> ((x,y),z)) [1..]
     where bound = 300
 
-getGridMap :: Int -> Map (Int,Int) Int
-getGridMap serial = Map.fromList $ map(\y -> (y,getPowerLevel serial y)) $ getGrid
+getNextSquare :: ((Int,Int),Int) -> [(Int,Int)]
+getNextSquare ((x,y), 1) = [(x,y)]
+getNextSquare ((x,y), size) = [(x+a,y+size-1) | a <- [0..size-1]] ++ [(x+size-1,y+a) | a <- [0..size-2]]
 
-getNext :: ((Int,Int),Int) -> [(Int,Int)]
-getNext ((x,y), 1) = [(x,y)]
-getNext ((x,y), size) = [(x+a,y+size-1) | a <- [0..size-1]] ++ [(x+size-1,y+a) | a <- [0..size-2]]
 
-day11bthree serial = maximumBy (\(_,_,a) (_,_,b) -> compare a b) $ map (\y ->(\(a,b) -> (y,a,b)) $ tryThree serial y) $ getGrid
--- tryThree :: Int -> (Int, Int) -> (Int, Int, Int)
-tryThree serial point = (\(_, b, _) -> b ) 
-                        $ foldr (\y (a,b,c) ->(\z -> (a+1, maxBySnd b (a+1,c+z), c+z)) $ (sumPowerLevels serial $ getNext y) ) (0,(0,0),0) 
+pointMaximum serial point = (\(_, b, _) -> b ) 
+                        $ foldr (\y (a,b,c) ->(\z -> (a+1, maxBySnd b (a+1,c+z), c+z)) $ (sumPowerLevels serial $ getNextSquare y) ) (0,(0,0),0) 
                         $ reverse 
                         $ squaresXByX point
-
--- tryThree serial point = foldr (\y (a,b,c) ->(\z -> (a+1, max b (c+z), c+z)) $ (sumPowerLevels 18 $ getNext y) ) (0,0,0) 
---                               $ reverse 
---                               $ squaresXByX point
+                        
 maxBySnd a b = case compare (snd a) (snd b) of
-                        GT -> a
-                        _  -> b
---gridMap = getGridMap 18
--- getLargestSquare serial point = foldr (\y acc -> get)
-
-glookup :: Map (Int,Int) Int -> (Int,Int) -> Int 
-glookup gmap loc = gmap Map.! loc
-
-
+                     GT -> a
+                     _  -> b
--- test/Day11Spec.hs
+++ test/Day11Spec.hs
@@ -41,30 +41,26 @@ spec = do
             let input2 =  (33,45)
             let expected = 29
             sumTbT input input2 `shouldBe` expected
-    describe "day11" $ do
-        it "should match given test case" $ do
-            let input = 18 
-            let expected = (33,45)
-            day11 input `shouldBe` expected
-        it "should match given test case" $ do
-            let input = 42
-            let expected = (21,61)
-            day11 input `shouldBe` expected
-    -- describe "day11b" $ do
+            -- these work but they're slow
+    -- describe "day11" $ do
     --     it "should match given test case" $ do
     --         let input = 18 
-    --         let expected = (90,(269,16))
-    --         day11b input `shouldBe` expected
-    describe "tryThree" $ do
+    --         let expected = (33,45)
+    --         day11 input `shouldBe` expected
+    --     it "should match given test case" $ do
+    --         let input = 42
+    --         let expected = (21,61)
+    --         day11 input `shouldBe` expected
+    describe "pointMaximum" $ do
         it " should give me test case 1" $ do 
             let serial = 18
             let point = (90,269)
             let expected = (16,113)
-            tryThree serial point `shouldBe` expected
+            pointMaximum serial point `shouldBe` expected
         it " should give me test case 2" $ do 
             let serial = 42
             let point = (232,251)
             let expected = (12,119)
-            tryThree serial point `shouldBe` expected
+            pointMaximum serial point `shouldBe` expected
         
         
